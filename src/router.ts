dbc30a859745fdd5e099a2a53e591b5a80d3db11
Daniel Rodrigues <danielprodrigues0@gmail.com>
2024-03-14 20:42:36 -0300

    refator: changes on router

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,12 @@
 import { Express } from "express";
+import { getAllUsers } from "./modules/user/user-controller";
 
-module.exports = (app: Express) => {
-  app.get("/", (_, res) => {
-    res.send("Hello World!");
-  });
+export default function (app: Express) {
+  app.get("/user", getAllUsers);
+  // app.get("/user/:id", getUserById);
+  // app.post("/user", createUser);
+  // app.put("/user/:id", updateUser);
+  // app.delete("/user/:id", deleteUser);
 
   return app;
-};
+}
